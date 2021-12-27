1cbff0d06f6cded30bc4e3813ba5c92ccf758244
ryanmstokes <ryanstoke@hotmail.com>
2021-12-26 19:23:46 -0600

    Create GroupingSelect component for first increment.
    [STATE:05] - Create GroupingSelect component for first increment.
    
     - Create TickeredList function to group deltas by single increment from 0.5 to 1.
     - Correct deletion of deltas set to 0 in asks and bids.
     - Correct ordering of asks deltas.

--- src/services/ws-manager/index.ts
+++ src/services/ws-manager/index.ts
@@ -1,17 +1,23 @@
 import { loadTables } from 'store/features/tables'
 import ListManager from 'utils/tables/list-manager'
 import { useDispatch } from 'react-redux'
+import TickeredList from 'utils/tickered-list'
+
+let UILoaded = false
+let asks: number[][] = []
+let bids: number[][] = []
 
 const WsManager = () => {
   const dispatch = useDispatch()
 
-  let UILoaded = false
-  let asks: number[][] = []
-  let bids: number[][] = []
-
   let interval: ReturnType<typeof setInterval>
   const setUpUIUpdate = () => {
-    interval = setInterval(() => dispatch(loadTables({ asks, bids })), 2000)
+    interval = setInterval(() => {
+      dispatch(loadTables({
+        asks: [...TickeredList(asks).slice(0, 15)],
+        bids: [...TickeredList(bids).slice(0, 15)]
+      }))
+    }, 1000)
   }
 
   const ws = new WebSocket("wss://www.cryptofacilities.com/ws/v1")
@@ -28,12 +34,12 @@ const WsManager = () => {
     try {
       if ((json.event = "data")) {
         if (json.feed === "book_ui_1_snapshot") {
-          asks = json.asks
-          bids = json.bids
+          asks = [...json.asks]
+          bids = [...json.bids]
         }
         if (json.feed === "book_ui_1") {
-          asks = [...ListManager(asks, json.asks)]
-          bids = [...ListManager(bids, json.bids)]
+          asks = [...ListManager(asks, json.asks, "ASC")]
+          bids = [...ListManager(bids, json.bids, "DESC")]
           if (!UILoaded) {
             setUpUIUpdate()
             UILoaded = true
